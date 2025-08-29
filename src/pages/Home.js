7230559898e009b0f697d83802975ad465054dba
Majk <michalliptak40@gmail.com>
2025-08-29 22:40:19 +0200

    add carousel to HP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,46 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import '../styles/Home.css'
+import carouselImg1 from '../assets/tasks.jpg';
+import carouselImg2 from '../assets/timetable.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Home() {
-    const [isSigned, setIsSigned] = useState(0);
-    const navigate = useNavigate();
-
-
-    useEffect(() => {
-        const storedSigned = localStorage.getItem("isSigned");
-        if (storedSigned === "1") {
-            setIsSigned(1);
-        }
-    }, []);
-
-    function handleLogin() {
-        setIsSigned(1);
-        localStorage.setItem("isSigned", "1");
-        navigate('/');
-    }
-
     return (
         <>
-            {isSigned === 0 ? (
-    <>
-        <Header/>
-        <div className="container main-elemnet">
-            <h2 className="text-center">Vitaj!</h2>
-            <h3>Prosím, prihlás sa</h3>
-            <button onClick={handleLogin}>
-                Prihlásiť sa
-            </button>
-        </div>
-    </>
-) : (
-    <>
-        <Header 
-            isSigned={isSigned} 
-            handleLogout={() => {
-                setIsSigned(0);
-                localStorage.removeItem("isSigned");
-            }} 
-        />
-        <h2>Si prihlásený, vitaj</h2>
-    </>
-)}
+            <Header />
+            <div className="container main-element">
+                <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <img src={carouselImg1} className="d-block w-100 img-carousel" alt="tasks"/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={carouselImg2} className="d-block w-100 img-carousel" alt="timetable"/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
